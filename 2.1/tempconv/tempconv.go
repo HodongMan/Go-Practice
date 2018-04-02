f687e57637235db6556380797bfa6b0c7eb17cba
hodongman <arshavin3@naver.com>
2018-04-03 01:21:14 +0900

    bug fixed

--- 2.1/tempconv/tempconv.go
+++ 2.1/tempconv/tempconv.go
@@ -2,7 +2,7 @@ package tempconv
 
 import "fmt"
 
-type Celcius float64
+type Celsius float64
 type Fahrenheit float64
 type Kelvin float64
 
@@ -13,6 +13,6 @@ const (
     ZeroKelvin Celsius = 0
 )
 
-func (c Celcius) String() string { return fmt.Sprintf("%gC", c) }
+func (c Celsius) String() string { return fmt.Sprintf("%gC", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%gF", f) }
 func (k Kelvin) String() string { return fmt.Sprintf("%gK", k)}
